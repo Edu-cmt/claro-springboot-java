c13448d5f7cf520b1eb588932fb4044d10312d4f
Eduardo Mota <educm1298@gmail.com>
2024-11-15 18:21:07 -0300

    Initial commit

---
+++ src/main/java/me/dio/ClaroSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroSpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroSpringbootJavaApplication.class, args);
+	}
+
+}
