793fb8021db7e1c1a3368fdb16e982461c64fc93
Eduardo Mota <educm1298@gmail.com>
2024-11-15 22:26:51 -0300

    Domain layer implementation

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClaroSpringbootJavaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClaroSpringbootJavaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
